7060d7092763e882914f30eb1be460361a83aa02
Barekaev Dmitry <barekaev@bk.ru>
2021-11-19 20:27:16 +0700

    Изменена кнопка с настройками на ToggleButton

--- app/src/main/java/ru/gb/student/androidcalculator/MainActivity.java
+++ app/src/main/java/ru/gb/student/androidcalculator/MainActivity.java
@@ -2,7 +2,9 @@ package ru.gb.student.androidcalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.ToggleButton;
 
 import ru.gb.student.androidcalculator.logic.CalcLogicVisual;
 
@@ -10,11 +12,25 @@ public class MainActivity extends AppCompatActivity {
 
     private CalcLogicVisual visual;
 
+    // Имя настроек
+    private static final String NameSharedPreference = "CalculatorAppKeyboardTheme";
+
+    // Имя параметра в настройках
+
+
+    private static final String appTheme = "APP_THEME";
+    private static final int DefaultTheme = 0;
+    private static final int CircleKeyboardTheme = 1;
+
+    private ToggleButton changeKeyboardTheme;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         visual = new CalcLogicVisual(findViewById(R.id.screen));
+
     }
+
 }
